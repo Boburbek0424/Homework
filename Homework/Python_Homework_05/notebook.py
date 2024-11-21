6c0d1d373f9a962d01ef8acfda283e48efba9438
Boburbek0424 <boburbekhakimov04@gmail.com>
2024-11-21 12:48:05 +0500

    Modified

--- Homework/Python_Homework_05/notebook.py
+++ Homework/Python_Homework_05/notebook.py
@@ -24,7 +24,6 @@ def display_notes(notes):
     if not notes:
         print("\nNo notes available.\n")
     else:
-        print("\nNotebook:")
         for note in notes:
             print(f"{note['id']}- '{note['text']}'")
         print()
@@ -36,7 +35,6 @@ def create_note(notes):
     notes.append({"id": note_id, "text": text})
     save_notes(notes)
     print("Note created successfully.")
-    display_notes(notes)
 
 # Update a note
 def update_note(notes):
@@ -47,7 +45,6 @@ def update_note(notes):
             note["text"] = new_text
             save_notes(notes)
             print("Note updated successfully.")
-            display_notes(notes)
             return
     print("Note not found.")
 
@@ -59,36 +56,30 @@ def delete_note(notes):
             notes.remove(note)
             save_notes(notes)
             print("Note deleted successfully.")
-            display_notes(notes)
             return
     print("Note not found.")
 
 # Main menu
 def main_menu():
     print("\nNotebook Application")
-    print("1. Create Note")
-    print("2. Update Note")
-    print("3. Delete Note")
-    print("4. Show Note Details")
-    print("5. Show All Notes")
+    print("1. Show all notes")
+    print("2. Show note details")
+    print("3. Creare note")
+    print("4. Update note")
+    print("5. Delete note")
     print("Q. Quit")
     print("M. Show Menu Again")
 
 # Main application logic
 def main():
     notes = load_notes()
-    main_menu()
     while True:
         choice = input("Enter your choice: ").strip().upper()
 
         if choice == "1":
-            create_note(notes)
+            display_notes(notes)
+            
         elif choice == "2":
-            update_note(notes)
-        elif choice == "3":
-            delete_note(notes)
-        elif choice == "4":
-            note_id = input("Enter note ID: ")
             for note in notes:
                 if note["id"] == note_id:
                     print(f"ID: {note['id']}")
@@ -96,15 +87,20 @@ def main():
                     break
             else:
                 print("Note not found.")
+            update_note(notes)
+        elif choice == "3":
+            create_note(notes)
+        elif choice == "4":
+            note_id = input("Enter note ID: ")
         elif choice == "5":
-            display_notes(notes)
+            delete_note(notes)
         elif choice == "Q":
             print("Exiting... Goodbye!")
             break
-        elif choice == "M":
+        elif choice == "M" or choice=='MENU':
             main_menu()
         else:
             print("Invalid choice. Please try again.")
-
+main_menu()
 if __name__ == "__main__":
     main()
